47732ac1082918ecc0475432ddb1ea78e91c51cd
Mobeen Raja <mobeen169@googlemail.com>
2025-07-27 21:20:31 +0100

    Creation of new concept "Categories", and creation of a new class called budget, deciding where to put the main method to run the programs and how to implement categories, basic concepts have been commited

--- Tracker.py
+++ Tracker.py
@@ -12,8 +12,22 @@ class Tracker:
         print("New Balance: {}".format(self.balance))
     
     def __repr__(self):
-        return "Balance: {balance}\nBudget: {budget}\nIncome: {income}".format(income = self.income, balance = self.balance, budget = self.budget)
-        
+        return "Balance: {balance}\nBudget: {budget}\nIncome: {income}\nCategories: {categories}".format(income = self.income, balance = self.balance, budget = self.budget, categories = self.categories)
+
+    def category(self):
+        self.categories = {}
+        while(True):
+            category = input("Enter a category (or enter X to return to main menu): ")
+            if category.lower() == "x":
+                break
+            if category in self.categories.keys():
+                print("Category already exists, please enter a different category (or enter x to return to main menu)")
+            else:
+                self.categories[category] = 0
+                print("Category added: {}".format(category))
+                break
+
 test_object = Tracker(300, 400, 120)
 test_object.update_balance(-300)
+test_object.category()
 print(test_object)
